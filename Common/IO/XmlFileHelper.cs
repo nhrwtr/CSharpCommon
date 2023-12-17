03e649c71502be7f0dd6d69058b7a606834dcb78
nhrwtr <nhrwtr.21f98@gmail.com>
2023-12-17 23:24:42 +0900

    - XMLファイル読み込みヘルパクラス追加 - README追加

---
+++ Common/IO/XmlFileHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Common.IO
+{
+    /// <summary>
+    /// XMLファイル読み込みのヘルパクラス
+    /// </summary>
+    public static class XmlFileHelper
+    {
+        /// <summary>
+        /// XMLファイルから、XMLドキュメント(<see cref="XDocument"/>を生成して返す
+        /// </summary>
+        /// <param name="filePath">ファイル名・およびパス</param>
+        /// <returns>XMLドキュメント</returns>
+        public static XDocument ReadDocument(string filePath)
+        {
+            return XDocument.Load(filePath);
+        }
+
+        /// <summary>
+        /// <see cref="XmlSerializer"/>を用いて、デシリアライズする
+        /// </summary>
+        /// <typeparam name="T">シリアライズする型</typeparam>
+        /// <param name="filePath">ファイル名・およびパス</param>
+        /// <returns>シリアライズされたオブジェクト</returns>
+        public static T Serialize<T>(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException("filePath", "null");
+            }
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException(@"File does not exist", "filePath");
+            }
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            T instance = default;
+            StreamReader sr;
+            using (sr = new StreamReader(filePath, Encoding.UTF8))
+            {
+                object obj = serializer.Deserialize(sr);
+                if (obj is T t)
+                {
+                    instance = t;
+                }
+            }
+
+            return instance;
+        }
+
+        /// <summary>
+        /// <see cref="DataContractSerializer"/>を用いて、デシリアライズする
+        /// </summary>
+        /// <typeparam name="T">シリアライズする型</typeparam>
+        /// <param name="filePath">ファイル名・およびパス</param>
+        /// <returns>シリアライズされたオブジェクト</returns>
+        public static T ContractSerialize<T>(string filePath)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+
+            T instance = default;
+
+            XmlReader sr;
+            using (sr = XmlReader.Create(filePath))
+            {
+                object obj = serializer.ReadObject(sr);
+                if (obj is T t)
+                {
+                    instance = t;
+                }
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,25 @@
+## Description
+
+C#であったら便利そうな基本的な機能をまとめた汎用ライブラリです。
+独自性もなく誰でも思いつく処理だと思うので、パブリックドメインとします。
+問題がある場合は、issueか下記ライセンスに記載のメールアドレスから連絡ください。
+
+It is a generic library of basic functions useful in C#.
+It is not proprietary and is in the public domain because it is a process that anyone can come up with.
+If you have any problems, please contact us via issue or the email address in the license section below.
+
+## Environments
+- NET libraries for .NET Standard 2.0
+This does not guarantee operation in all devices and situations.
+
+## Lincense
+
+CSharpCommon - This project is a collection of basic processes commonly used in C#.
+
+Written in 2023 by nhrwtr nhrwtr.21f98@gmail.com
+
+[other author/contributor lines as appropriate]
+
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
